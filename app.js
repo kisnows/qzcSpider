56514d8d6187e1138068325167b85377d69b510f
kisnows <yq12315@gmail.com>
2016-09-13 23:30:47 +0800

    添加保存每次解析后的json格式留言到本地

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
 const fs = require('fs')
 const fetchHTML = require('./controller/fetchHtml')
 const getCommentList = require('./controller/parseHTML')
-const Comment = require('./db/modal')
+const Comment = require('./db')
+const moment = require('moment')
+const start = moment()
 
-fetchHTML()
-  .then(data=>storeComment(data))
-  .catch(err=>console.error(err))
+function main() {
+  fetchHTML()
+    .then(data=>storeComment(data))
+    .then(()=> {
+      const end = moment()
+      console.log(`Spend ${start.from(end)}`)
+    })
+    .catch(err=>console.error(err))
+}
+
+main()
+
 
 /**
  * 存储 comment 到数据库
  * @param data
  */
 function storeComment(data) {
-  getCommentList(data)
-    .then(commentList=> {
-      let fileName = `./data/comment-${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}.json`
-      fs.writeFile(fileName, JSON.stringify(commentList), err=> {
-        console.error(err)
+  return new Promise((resolve, reject)=> {
+    getCommentList(data)
+      .then(commentList=> {
+        let fileName = `./data/comment-${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}.json`
+        fs.writeFile(fileName, JSON.stringify(commentList), err=> {
+          console.error(err)
+        })
+        commentList.forEach((v, k)=> {
+          const comment = new Comment(v)
+          comment.pureSave(comment)
+            .then(()=> {
+              if (k === commentList.length - 1) {
+                console.log(`Process end,saved ${k} comments`)
+                resolve()
+                process.exit()
+              }
+            })
+            .catch(err=>reject(err))
+        })
       })
-      commentList.forEach((v, k)=> {
-        const comment = new Comment(v)
-        comment.pureSave(comment)
-          .then(()=> {
-            if (k === commentList.length - 1) {
-              console.log(`Process end,saved ${k} comments`)
-              process.exit()
-            }
-          })
-          .catch(err=>console.error(err))
-      })
-    })
-    .catch(err=>console.error(err))
+      .catch(err=>reject(err))
+  })
 }
